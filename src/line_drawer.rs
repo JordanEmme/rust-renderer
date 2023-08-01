3d4836ec618249390f07884ddd3cf7975fc424ae
Jordan Emme <jordanemme@protonmail.com>
2023-08-01 22:05:13 +0200

    implementing Bresenham line drawing

--- src/line_drawer.rs
+++ src/line_drawer.rs
@@ -1,16 +1,40 @@
 use super::tga;
-use std::iter;
 
-pub fn draw_line(x0: u16, y0: u16, x1: u16, y1: u16, img: &mut tga::Image<tga::Rgb>, color: tga::Rgb) {
-    iter::successors(Some(0f64), |i| {
-        let next: f64 = i + 0.1f64;
-        (next < 1f64).then_some(next)
-    })
-    .for_each(|step| {
-        let pixel_x: u16 = x0 + ((x1 - x0) as f64 * step) as u16;
-        let pixel_y: u16 = y0 + ((y1 - y0) as f64 * step) as u16;
-        let _ = img.set(pixel_x, pixel_y, color);
-    })    
+pub fn line(x0: u16, y0: u16, x1: u16, y1: u16, img: &mut tga::Image<tga::Rgb>, color: tga::Rgb) {
+    let mut start_x: u16 = x0;
+    let mut start_y: u16 = y0;
+    let mut end_x: u16 = x1;
+    let mut end_y: u16 = y1;
+
+    let mut steep: bool = false; 
+    if x0.abs_diff(x1) < y0.abs_diff(y1) { 
+        (start_x, start_y) = (start_y, start_x);
+        (end_x, end_y) = (end_y, end_x);
+        steep = true; 
+    } 
+    if start_x > end_x { 
+        (start_x, end_x) = (end_x, start_x);
+        (start_y, end_y) = (end_y, start_y);
+    } 
+    let dx: u32 = (end_x - start_x) as u32; 
+    let derror2: u32 = end_y.abs_diff(start_y) as u32 * 2; 
+    let mut error2: u32 = 0u32; 
+    let mut y: u16 = start_y;
+    for x in start_x..end_x { 
+        if steep { 
+            let _ = img.set(y, x, color); 
+        } else { 
+            let _ = img.set(x, y, color); 
+        } 
+        error2 += derror2; 
+        if error2 > dx { 
+            if end_y > start_y {
+                y += 1;
+            }
+            else {
+                y -= 1;
+            }
+            error2 -= dx * 2; 
+        } 
+    } 
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ const RED: tga::Rgb = tga::Rgb {r: 255, g: 0, b: 0};
 
 fn main() {
     let mut img = tga::Image::<tga::Rgb>::new(10,10);
-    line_drawer::draw_line(0, 0, 9,9, &mut img, RED);
+    line_drawer::line(0, 0, 9,9, &mut img, RED);
 
 //    let mut img = tga::Image::<tga::Rgb>::new(10, 10);
 //    let _ = img.set(0, 0, RED);
